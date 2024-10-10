1f65d10cffde110e20a4303ed248cc02eeccca65
Neal Magee <nem2p@virginia.edu>
2024-10-10 16:33:08 +0000

    Tracking with error handling and deeper logging

--- app.py
+++ app.py
@@ -4,17 +4,23 @@ import requests
 import json
 import logging
 
-url = "https://official-joke-api.appspot.com/random_joke"
-
-response = requests.get(url)
-response = json.loads(response.text)
-
-id = response['id']
-setup = response['setup']
-punchline = response['punchline']
-
-# print(id, setup, punchline)
-
 logger = logging.getLogger(__name__)
 logging.basicConfig(filename="joke.log", encoding="utf-8", level=logging.DEBUG)
-logging.warning('%s:%s:%s', id, setup, punchline)
+
+url = "https://official-joke-api.appspot.com/random_joke"
+
+try:
+    # get the payload
+    response = requests.get(url)
+    # put it into json format
+    response = json.loads(response.text)
+    # fetch each data point by key
+    id = response['id']
+    setup = response['setup']
+    punchline = response['punchline']
+    # log the data
+    logging.warning('%s:%s:%s', id, setup, punchline)
+except Exception as e:
+    # log the error
+    logging.error(e)
+
